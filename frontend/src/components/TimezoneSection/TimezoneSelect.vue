1777a267bc21c8f28ac204d6d33cc891e9339552
BrillNash <brillnash.piner@gmail.com>
2025-06-11 22:42:40 +0800

    Add TimezoneSection components – index and TimezoneSelect

---
+++ frontend/src/components/TimezoneSection/TimezoneSelect.vue
@@ -0,0 +1,21 @@
+<template>
+  <Select>
+    <SelectTrigger class="w-full border rounded-b-md">
+      <SelectValue :placeholder="timezoneStore.timezone !== '' ? timezoneStore.timezone : 'Select a timezone'" />
+    </SelectTrigger>
+    <SelectContent class="max-h-60 overflow-y-auto">
+      <SelectGroup v-for="timezone in timezones" :key="timezone.value">
+        <SelectItem :value="timezone.value">
+          {{ timezone.label }}
+        </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+</template>
+
+<script lang="ts" setup>
+import { timezones } from '@/constants/timezone';
+import { useTimezoneStore } from '@/stores/timezone';
+
+const timezoneStore = useTimezoneStore()
+</script>
---
+++ frontend/src/components/TimezoneSection/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <Card class="w-full h-full rounded-lg border">
+      <CardHeader>
+        <CardTitle>Timezone Settings</CardTitle>
+        <CardDescription>Preffered Timezone</CardDescription>
+      </CardHeader>
+      <CardContent class="flex flex-col gap-4">
+        <TimezoneSelect v-model="updatedTimezone"/>
+        <Button @click="update()" class="px-4 py-2 w-fit rounded-sm">Update Timezone</Button>
+      </CardContent>
+    </Card>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import TimezoneSelect from './TimezoneSelect.vue'
+import { useTimezoneStore } from '@/stores/timezone'
+import { ref } from 'vue'
+
+const timezoneStore = useTimezoneStore()
+
+const updatedTimezone = ref(timezoneStore.timezone)
+
+const update = async () => {
+  await timezoneStore.updateTimezone(updatedTimezone.value)
+}
+</script>

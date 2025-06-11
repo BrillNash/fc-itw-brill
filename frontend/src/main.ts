a7654ffb542d9371d64be523f82072fc1f5bbd72
BrillNash <brillnash.piner@gmail.com>
2025-06-11 22:45:02 +0800

    Update App.vue and main.ts for application setup adjustments

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import registerGlobalComponents from './plugins/registerGlobalComponents'
+import ErrorHandlerPlugin from './plugins/errorHandler'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(ErrorHandlerPlugin)
+
+registerGlobalComponents(app)
 
 app.mount('#app')

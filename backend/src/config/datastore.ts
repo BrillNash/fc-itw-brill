ecfd054ea757b9fa1d7193af00e8d2996c101ea1
BrillNash <102124031+BrillNash@users.noreply.github.com>
2025-06-13 13:41:52 +0800

    Update datastore config

--- backend/src/config/datastore.ts
+++ backend/src/config/datastore.ts
@@ -2,8 +2,10 @@ import dotenv from 'dotenv'
 import { Datastore } from "@google-cloud/datastore";
 dotenv.config()
 
-export const datastore = new Datastore({
-  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  databaseId: 'test-database'
-})
+export const datastore = process.env.NODE_ENV === 'production'
+? new Datastore()
+: new Datastore({
+    projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    databaseId: process.env.GOOGLE_DATABASE_ID
+  });
